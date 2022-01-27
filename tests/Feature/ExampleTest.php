8fbc38f9c2ec882b95a78c8c65d39c8b59b95db5
Ismail Kamran <U1857977@unimail.hud.ac.uk>
2022-01-27 00:07:59 +0000

    tests written in exampleTestsFile

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,21 +12,87 @@ use App\Models\User;
 class ExampleTest extends TestCase
 {
 
-    use RefreshDatabase;
+    
+        use RefreshDatabase;
+    
+        /** @test */
+        public function create_book_test()
+        {
+            $response = $this->postJson(route('book.store'), [
+                'name' => "TEST NAME",
+                'description' => "TEST DESCRIPTION"
+            ]);
+    
+            $response->assertCreated();
+    
+            $this->assertEquals('TEST NAME', $response->json()['name']);
+            $this->assertEquals('TEST DESCRIPTION', $response->json()['description']);
+        }
+    
+    
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function delete_book()
     {
-        $response = $this->get('/');
+         $book = Book::factory()->create([
+             'name' => "TEST NAME",
+             'description' => "TEST DESCRIPTION"
+         ]);
+ 
+         $response = $this->delete(route('books.destroy', $book->id));
+ 
+         $response->assertNoContent();
+    }
+ 
 
-        $response->assertStatus(200);
+    public function show_all_todo()
+    {
+        $book = Book::factory()->create([
+            'name' => "TEST NAME",
+            'description' => "TEST DESCRIPTION"
+        ]);
+
+        $response = $this->getJson(route('todo.index'));
+
+        $response->assertOk();
+
+        $this->assertCount(1, $response->json());
+
+        $this->assertEquals('TEST NAME', $response->json()[0]['name']);
+        $this->assertEquals('TEST DESCRIPTION', $response->json()[0]['description']);
     }
 
+    public function show_todo()
+    {
+        $book = Book::factory()->create([
+            'name' => "TEST NAME",
+            'description' => "TEST DESCRIPTION"
+        ]);
+
+        $response = $this->getJson(route('todo.show', $book->id));
+
+        $response->assertOk();
+
+        $this->assertEquals('TEST NAME', $response->json()['name']);
+        $this->assertEquals('TEST DESCRIPTION', $response->json()['description']);
+    }
+
+    public function update_todo_test()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->putJson(route('books.update', $book->id), [
+            'name' => "TEST NAME",
+            'description' => "TEST DESCRIPTION"
+        ]);
+
+        $response->assertOk();
+
+        $this->assertEquals('TEST NAME', $response->json()['name']);
+        $this->assertEquals('TEST DESCRIPTION', $response->json()['description']);
+    }
+ }
+
     
 
     
-}
+
